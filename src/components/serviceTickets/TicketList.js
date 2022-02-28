157f5a698424ab04b6340c1ceaf187775b9cba26
Gregory Haney <gregory.haney@gmail.com>
2022-02-28 13:04:57 -0600

    Complete, up through Ch 7

---
+++ src/components/serviceTickets/TicketList.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react"
+
+export const TicketList = () => {
+    const [tickets, updateTickets] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/serviceTickets?_expand=customer&_expand=employee")
+            .then(res => res.json())
+            .then((data) => {
+                updateTickets(data)
+            })
+        },
+        []
+    )
+
+
+    return (
+    <>
+        {
+            tickets.map(
+                (ticket) => {
+                    return <div key={`ticket--${ticket.id}`}>
+                        <p>{ticket.description} submitted by {ticket.customer.name}
+                        and worked on by {ticket.employee.name}       
+                        </p>
+                        </div>
+                }
+            )
+        }
+    </>
+    )
+}
